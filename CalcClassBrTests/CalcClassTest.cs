8d5a5a6bf3e759922f6307d327f93adeb8c18fb7
AnnPilchak <anya.pilchak.03@gmail.com>
2022-10-04 14:39:34 +0300

    Добавьте файлы проекта.

---
+++ CalcClassBrTests/CalcClassTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CalcClassBr;
+using System.Data.SqlTypes;
+
+namespace CalcClassBr.Tests
+{
+    [TestClass]
+    public class CalcClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                    "test.xml",
+                    "Operation", DataAccessMethod.Sequential)]
+
+        public void DivTest()
+        {
+            //Arrange
+            long expression_1 = Convert.ToInt64(TestContext.DataRow["expression_1"]);
+            long expression_2 = Convert.ToInt64(TestContext.DataRow["expression_2"]);
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+
+            //Act
+            string result;
+            try
+            {
+                result = Convert.ToString(CalcClass.Div(expression_1, expression_2));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                result = ex.ParamName;
+            }
+            catch (DivideByZeroException e)
+            {
+                result = e.Message;
+            }
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ CalcClassBrTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcClassBrTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcClassBrTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("353ad921-d80d-4594-b006-82aa29234bcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
